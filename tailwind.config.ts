13aeac00b3f857c5139ed7f9b8b6df729c5360b1
Devaditya Dayal <rainlarcade@gmail.com>
2025-08-15 14:22:55 +0000

    Are the changes that you were applying before also included?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -103,7 +103,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'caret-blink': 'caret-blink 1.25s ease-out infinite'
+        'caret-blink': 'caret-blink 1.2s infinite'
       },
     },
   },
